0aa052cb195f17fc50dbdade84c228e89a6768f7
FilipaRamos <fimamora@gmail.com>
2015-05-12 18:16:51 +0100

    alterações no background

---
+++ src/game/cli/Background.java
@@ -0,0 +1,58 @@
+package game.cli;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+public class Background {
+	
+	private ImageIcon background;
+	private int x;
+	private int y;
+	
+	// CONSTRUTOR SEM ARGUMENTOS
+	public Background(){
+		x = 0;
+		y = 0;
+	}
+	
+	// CONSTRUTOR
+	public Background(int x_position, int y_position){
+		x = x_position;
+		y = y_position;
+		
+		background = new ImageIcon("C:/Users/Filipa/workspace/Super-Mario/resources/background.png");
+	}
+	
+	// DESENHAR A IMAGEM DE FUNDO
+	public void draw(){
+		// mover a imagem
+		x -= 5;
+		// verificar se a imagem saiu do ecrã
+		if (x <= -1*getWidth()){
+			// ajustar se sair
+			x += getWidth() * 2;
+		}
+			
+	}
+	
+	// DEVOLVER A POSIÇÃO DO X
+	public int getX(){
+		return x;
+	}
+	
+	// DEVOLVER A POSIÇÃO DO Y
+	public int getY(){
+		return y;
+	}
+	
+	// DEVOLVER O COMPRIMENTO DA IMAGEM
+	public int getWidth(){
+		return background.getIconWidth();
+	}
+	
+	
+}
--- src/game/cli/MovingBackground.java
+++ src/game/cli/MovingBackground.java
@@ -1,57 +1,12 @@
 package game.cli;
+import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 
 public class MovingBackground {
-	
-	private ImageIcon background;
-	private int x;
-	private int y;
-	
-	// CONSTRUTOR SEM ARGUMENTOS
-	public MovingBackground(){
-		x = 0;
-		y = 0;
-	}
-	
-	// CONSTRUTOR
-	public MovingBackground(int x_position, int y_position){
-		x = x_position;
-		y = y_position;
-		
-		background = new ImageIcon("C:/Users/Filipa/workspace/Super-Mario/resources/background.png");
-	}
-	
-	public void draw(){
-		x -= 5;
-		
-		if (x <= -1*getWidth()){
-			x = x + getWidth() * 2;
-		}
-			
-	}
-	
-	// DEVOLVER A POSIÇÃO DO X
-	public int getX(){
-		return x;
-	}
-	
-	// DEVOLVER A POSIÇÃO DO Y
-	public int getY(){
-		return y;
-	}
-	
-	// DEVOLVER O COMPRIMENTO DA IMAGEM
-	public int getWidth(){
-		return background.getIconWidth();
-	}
-	
-	
+
 	
 	
 }

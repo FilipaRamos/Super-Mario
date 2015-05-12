f17bf82a16d46cfe6fbdff5f5f4ff981b647af05
FilipaRamos <fimamora@gmail.com>
2015-05-12 18:07:13 +0100

    inicio do moving background

--- src/game/cli/MovingBackground.java
+++ src/game/cli/MovingBackground.java
@@ -5,37 +5,52 @@ import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 
 public class MovingBackground {
 	
-	private BufferedImage background;
+	private ImageIcon background;
 	private int x;
 	private int y;
 	
+	// CONSTRUTOR SEM ARGUMENTOS
 	public MovingBackground(){
 		x = 0;
 		y = 0;
 	}
 	
+	// CONSTRUTOR
 	public MovingBackground(int x_position, int y_position){
 		x = x_position;
 		y = y_position;
 		
-		// TENTAR LER A IMAGEM DO BACKGROUND
-		try {
-			background = ImageIO.read(new File("background.png"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		background = new ImageIcon("C:/Users/Filipa/workspace/Super-Mario/resources/background.png");
 	}
 	
+	public void draw(){
+		x -= 5;
+		
+		if (x <= -1*getWidth()){
+			x = x + getWidth() * 2;
+		}
+			
+	}
+	
+	// DEVOLVER A POSIÇÃO DO X
 	public int getX(){
 		return x;
 	}
 	
+	// DEVOLVER A POSIÇÃO DO Y
 	public int getY(){
 		return y;
 	}
+	
+	// DEVOLVER O COMPRIMENTO DA IMAGEM
+	public int getWidth(){
+		return background.getIconWidth();
+	}
+	
 	
 	
 	
